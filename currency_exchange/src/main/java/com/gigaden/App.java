2d6f0200aae24a582030087a76d0fd628882c0bb
gigaden <gigaden@ya.ru>
2025-09-24 21:47:28 +0300

    поправлен конвертер, добавлен цикл с выходом

--- currency_exchange/src/main/java/com/gigaden/App.java
+++ currency_exchange/src/main/java/com/gigaden/App.java
@@ -3,9 +3,6 @@ package com.gigaden;
 import com.gigaden.service.CurrencyConverter;
 import com.gigaden.utils.ExchangeRates;
 
-import java.math.BigDecimal;
-import java.util.Scanner;
-
 /**
  * Курсы валют зашил в отдельный файл.
  * Для большей точности решил использовать BigDecimal
@@ -13,16 +10,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         CurrencyConverter converter = new CurrencyConverter(ExchangeRates.RATES);
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Доступные валюты: " + ExchangeRates.RATES.keySet());
-
-        System.out.print("Введите сумму: ");
-        BigDecimal amount = sc.nextBigDecimal();
-
-        System.out.print("Введите валюту (например, USD, EUR, GBP, JPY, RUB): ");
-        String currency = sc.next().toUpperCase();
-
-        converter.convert(amount, currency);
+        converter.start();
     }
 }
--- currency_exchange/src/main/java/com/gigaden/service/CurrencyConverter.java
+++ currency_exchange/src/main/java/com/gigaden/service/CurrencyConverter.java
@@ -3,12 +3,44 @@ package com.gigaden.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Map;
+import java.util.Scanner;
 
 public class CurrencyConverter {
     private final Map<String, BigDecimal> rates;
 
     public CurrencyConverter(Map<String, BigDecimal> rates) {
         this.rates = rates;
+    }
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Бро, добро пожаловать в конвертер валют!");
+        System.out.println("Доступные валюты: " + rates.keySet());
+        System.out.println("Надоело конвертировать? Введи exit");
+
+        while (true) {
+            System.out.print("\nВведи сумму: ");
+            String input = sc.next();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("До встречи, ещё увидимся!)");
+                break;
+            }
+
+            BigDecimal amount;
+            try {
+                amount = new BigDecimal(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Хватит меня ломать, вводи число!.");
+                continue;
+            }
+
+            System.out.print("Введи валюту (например, USD, EUR, GBP, JPY, RUB): ");
+            String currency = sc.next().toUpperCase();
+
+            convert(amount, currency);
+        }
     }
 
     public void convert(BigDecimal amount, String fromCurrency) {
